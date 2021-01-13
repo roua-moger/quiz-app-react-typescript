45d2801119b3066ee2ae9a77a1767f2d74edec33
Rowaa <rowaa.almogheer@gmail.com>
2021-01-13 13:12:28 +0100

    style completed

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 //Types
 import { AnswerObject } from '../App';
 //Styles 
-
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 
 
 type Props = {
@@ -16,22 +16,26 @@ type Props = {
 
 const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNr, totalQuestions
 }) => (
-     <div>
+     <Wrapper>
         <p className="number">
           Question: {questionNr} / {totalQuestions}  
         </p>
         <p dangerouslySetInnerHTML={{__html: question}} />
         <div>
             {answers.map(answer => (
-                <div key = {answer}>
+                <ButtonWrapper 
+                key = {answer}
+                correct = {userAnswer?.correctAnswer === answer}
+                userClicked = {userAnswer?.answer === answer}
+                >
                     <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{__html: answer}} />
                     
                     </button>
-                </div>
+                </ButtonWrapper>
             ))}
         </div>
-     </div>
+     </Wrapper>
      );
 
 export default QuestionCard;
